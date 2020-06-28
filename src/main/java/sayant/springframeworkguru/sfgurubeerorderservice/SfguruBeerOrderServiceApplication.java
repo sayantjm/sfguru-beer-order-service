97fb8079cf5f58af1f0c2ff9c4adf7b559683d83
Juanma Perales <sayantjm@gmail.com>
2020-06-28 09:51:53 +0200

    Initial commit . Closes #1

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerorderservice/SfguruBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package sayant.springframeworkguru.sfgurubeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfguruBeerOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfguruBeerOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sayant/springframeworkguru/sfgurubeerorderservice/SfguruBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sayant.springframeworkguru.sfgurubeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfguruBeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
