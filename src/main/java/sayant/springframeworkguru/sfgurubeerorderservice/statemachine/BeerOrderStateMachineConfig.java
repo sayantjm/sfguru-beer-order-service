d5af807a5cfbee2b11bd1e137c67a22d821e4d38
Juanma Perales <sayantjm@gmail.com>
2021-02-06 09:27:36 +0100

    Setup Allocations State Machine States. Closes #19

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerorderservice/statemachine/BeerOrderStateMachineConfig.java
@@ -0,0 +1,29 @@
+package sayant.springframeworkguru.sfgurubeerorderservice.statemachine;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import sayant.springframeworkguru.sfgurubeerorderservice.domain.enums.BeerOrderEventEnum;
+import sayant.springframeworkguru.sfgurubeerorderservice.domain.enums.BeerOrderStatusEnum;
+
+import java.util.EnumSet;
+
+/**
+ * Created by sayantjm on 6/2/21
+ */
+@Configuration
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum>{
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.PICKED_UP)
+                .end(BeerOrderStatusEnum.DELIVERED)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+    }
+}
