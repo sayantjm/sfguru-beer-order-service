8a4d2a26d5df7c1b404d4158de743ff6577821aa
Juanma Perales <sayantjm@gmail.com>
2021-02-20 17:22:15 +0100

    Adding test for Allocation Failure and Partial allocation. Closes #34

--- src/test/java/sayant/springframeworkguru/sfgurubeerorderservice/service/testcomponents/BeerOrderAllocationListener.java
+++ src/test/java/sayant/springframeworkguru/sfgurubeerorderservice/service/testcomponents/BeerOrderAllocationListener.java
@@ -22,20 +22,37 @@ public class BeerOrderAllocationListener {
 
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
     public void listen(Message msg) {
+        boolean pendingInventory = false;
+        boolean allocationError = false;
 
         AllocateOrderRequest request = (AllocateOrderRequest) msg.getPayload();
 
-        System.out.println("########### BeerOrderAllocationListener RAN ########");
+        // set pending inventory
+        if (request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("partial-allocation")) {
+            pendingInventory = true;
+        }
 
+        // set allocation error
+        if (request.getBeerOrderDto().getCustomerRef() != null && request.getBeerOrderDto().getCustomerRef().equals("fail-allocation")) {
+            allocationError = true;
+        }
+
+
+        boolean finalPendingInventory = pendingInventory;
         request.getBeerOrderDto().getBeerOrderLines().forEach(beerOrderLineDto -> {
-            beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            if (finalPendingInventory) {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity() - 1);
+            } else {
+                beerOrderLineDto.setQuantityAllocated(beerOrderLineDto.getOrderQuantity());
+            }
+
         });
 
         jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
                 AllocateOrderResult.builder()
                         .beerOrderDto(request.getBeerOrderDto())
-                        .pendingInventory(false)
-                        .allocationError(false)
+                        .pendingInventory(pendingInventory)
+                        .allocationError(allocationError)
                         .build()
                 );
     }
